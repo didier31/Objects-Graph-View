e529bfd94c2d8893d7cae2584d37ec7f56638a9b
didier <didier@biscarrosse>
2016-12-24 11:15:46 +0100

    Corrections

--- ObjectGraphViewer/src/org/eclipse/contrib/debug/event/ChangeListener.java
+++
@@ -1,6 +0,0 @@
-package org.eclipse.contrib.debug.event;
-
-public interface ChangeListener<Action extends Enum<Action>, Subject extends Object> {
-
-	public void hasChanged(ChangeEvent<Action, Subject> e);
-}
